f02ff90a7e5a97e50e763bb2352ddc2367a0301d
Gary Dix <Gary.Dix@atcltd.atc.org.nz>
2023-06-26 13:19:27 +1200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div>
+
+      <header>
+        <h1>The Movie database</h1>
+        <h4>Search, and then click on a movie to see the plot.</h4>
       </header>
+
+      <main>
+
+
+      </main>
+
+
     </div>
+
   );
 }
 
